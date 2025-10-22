aa274ed897081e9d96dec80aaefeec0ea79f86a9
Maxim <m4xim.zemskov@gmail.com>
2025-10-22 13:47:18 +0300

    remove test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-def get_last_dataset():
-    file_names = []
-    files = [os.path.join("datasets", f) for f in os.listdir("datasets") if os.path.isfile(os.path.join("datasets", f))]
-    last_dataset = max(files, key=os.path.getmtime)
-    return last_dataset

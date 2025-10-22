c58f49acb3719d644d029bea410ae48432450fec
Maxim <m4xim.zemskov@gmail.com>
2025-10-23 01:00:56 +0300

    new prep

--- prep.py
+++ prep.py
@@ -15,12 +15,6 @@ stemmer = SnowballStemmer("russian")
 def now():
     return str(datetime.datetime.now()).replace(' ', '_').replace(':', '_')
 
-def preprocess_docs(raw_docs):
-    n_jobs = max(1, cpu_count() - 1)
-    with Pool(processes=n_jobs) as pool:
-        docs = list(tqdm(pool.imap(preprocessing, raw_docs), total=len(raw_docs)))
-    return docs
-
 def preprocessing(raw_text:str):
     raw_text = raw_text.lower()
     raw_text = re.sub(r'[^\w\s]|\d', '', raw_text) 
@@ -29,16 +23,26 @@ def preprocessing(raw_text:str):
     tokens = [stemmer.stem(i) for i in raw_text.split() if i not in stop_words]
     return tokens
 
+def preprocess_docs(batch):
+    texts = batch["text"]
+    processed = [preprocessing(i) for i in texts]
+    return {"tokens" : processed}
+
 if __name__ == '__main__':
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
 
+    dataset = load_dataset('0x7o/taiga', split='train')
     if config['dataset']['DEBUG']:   
-        dataset = debug_dataset
-    else:
-        dataset = load_dataset('0x7o/taiga', split='train')['text']
+        dataset = dataset[:1000]
 
-    docs = preprocess_docs(dataset)
-
-    with open(f'datasets/{now()}.pkl', 'wb') as file:
-            pickle.dump(docs, file)
+    
+    dataset = dataset.map(
+        preprocess_docs,
+        batched=True,
+        num_proc=max(1, cpu_count() - 1),
+        remove_columns=["text"],
+        desc="Preprocessing"
+    )
+    dataset.save_to_disk('preprocessed/')
+    
